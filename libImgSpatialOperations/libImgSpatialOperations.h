8b1eff7ae0b6dcfb0aa42c3b08e15643131a741f
Yanxiang Wu <cosmobserver@gmail.com>
2012-02-14 21:59:15 +0800

    experiment commit

---
+++ libImgSpatialOperations/libImgSpatialOperations.h
@@ -0,0 +1,4 @@
+// libImgSpatialOperating - Yanxiang Wu's implementation of many frequently
+// used spatial image operations
+// Copyright 2012 Yanxiang Wu. All right reserved
+// https://github.com/cecilulysess/libImgSpatialOperations
